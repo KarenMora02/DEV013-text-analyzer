a58bb78e45c0141fbace8b1311be6e8ecea39ede
karenmora02 <morak970@gmail.com>
2023-12-11 12:27:38 -0500

    contador  caracteres sin espacios

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,18 +1,29 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    //parto en espacios 
+    //parto en espacios trae palabras
     return text.split(" ").length;
   },
   getCharacterCount: (text) => {
     return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCountExcludingSpaces: (text) => {
+  getCharacterNoSpacesCount: (text) => {
+    return text.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").replace(/ /g, "")
+      .length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    text.split(" ");
+    const palabras = text.split(" "); 
+    for (const property in palabras) {
+      console.log(`${property}: ${palabras[property]}`);
+      
+      //tarer la longitud de cada palabras y sumarla a una variable que inicie en 0 al final del for dividir en la cantidad de palabras 
+    }
+     
+   
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.

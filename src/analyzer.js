b93f31708fe99b460301380efca75466b20cb564
karenmora02 <morak970@gmail.com>
2023-12-12 15:59:15 -0500

    longitud de palabras

--- src/analyzer.js
+++ src/analyzer.js
@@ -2,7 +2,7 @@ const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
     //parto en espacios trae palabras
-    return text.split(" ").length;
+    return text.split(" ").filter((el) => el !== "").length;
   },
   getCharacterCount: (text) => {
     return text.length;
@@ -16,14 +16,18 @@ const analyzer = {
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
     text.split(" ");
-    const palabras = text.split(" "); 
-    for (const property in palabras) {
-      console.log(`${property}: ${palabras[property]}`);
-      
-      //tarer la longitud de cada palabras y sumarla a una variable que inicie en 0 al final del for dividir en la cantidad de palabras 
+    const palabras = text.split(" ").filter((el) => el !== "");
+    let sumaLongitudes = 0;
+
+    //recorre
+    for (let i = 0; i < palabras.length; i++) {
+      sumaLongitudes += palabras[i].length;
     }
-     
-   
+    //calcula la longitud
+    const longitudMedia = sumaLongitudes / palabras.length;
+
+    return longitudMedia;
+    //tarer la longitud de cada palabras y sumarla a una variable que inicie en 0 al final del for dividir en la cantidad de palabras
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.

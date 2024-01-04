3bce6a02acd4975d79ce6ea41f4521099e176494
karenmora02 <morak970@gmail.com>
2024-01-04 11:33:53 -0500

    termino fun

--- src/analyzer.js
+++ src/analyzer.js
@@ -12,7 +12,7 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterNoSpacesCount: (text) => {
-    return text.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").replace(/ /g, "")
+    return text.replace(/[&/\\#,+()$~%.'":*?<>{}]/g, "").replace(/ /g, "")
       .length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
